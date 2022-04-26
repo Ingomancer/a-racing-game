3e6ed93249af6dd8942d2b727c4b22817f207d84
Ingo Junefjord <lord.ingo@gmail.com>
2022-04-26 20:40:51 +0200

    MULTIPLAYER SUPPORT OMG

--- data/car.py
+++ data/car.py
@@ -3,21 +3,22 @@ import pygame
 
 
 class Car:
-    def __init__(self, image):
+    def __init__(self, image, controls):
         self.image = image
         self.pos = image.get_rect()
         self.angle = 0
         self.speed = 5
+        self.up, self.down, self.right, self.left = controls
 
     def handle_keys(self, pressed):
         radians = math.radians(self.angle)
         x = self.speed * math.cos(radians)
         y = self.speed * math.sin(radians)
-        if pressed[pygame.K_UP] or pressed[pygame.K_w]:
+        if pressed[self.up]:
             self.pos = self.pos.move(x, -y)
-        if pressed[pygame.K_DOWN] or pressed[pygame.K_s]:
+        if pressed[self.down]:
             self.pos = self.pos.move(-x, y)
-        if pressed[pygame.K_RIGHT] or pressed[pygame.K_d]:
+        if pressed[self.right]:
             self.angle -= 5
-        if pressed[pygame.K_LEFT] or pressed[pygame.K_a]:
+        if pressed[self.left]:
             self.angle += 5
--- data/main.py
+++ data/main.py
@@ -1,27 +1,32 @@
-import sys
 import pygame
 from data.car import Car
 
 from data.graphics.graphics import load
 
+wasd_controls = (pygame.K_w, pygame.K_s, pygame.K_d, pygame.K_a)
+arrow_controls = (pygame.K_UP, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_LEFT)
 
 def main():
     screen = pygame.display.set_mode((640, 480))
-    player = Car(load("player.png", alpha=True))
+    players = []
+    players.append(Car(load("player_red.png", alpha=True), wasd_controls))
     background = load("grass.bmp")
     screen.blit(background, (0, 0))
-    position = player.pos
-    screen.blit(player.image, position)
+    for player in players:
+        position = player.pos
+        screen.blit(player.image, position)
     pygame.display.update()
     while True:
         pressed = pygame.key.get_pressed()
-        player.handle_keys(pressed)
+        for player in players:
+            player.handle_keys(pressed)
         if pressed[pygame.K_q]:
             break
         screen.blit(background, (0, 0))
-        rotated_player = pygame.transform.rotate(player.image, player.angle)
-        rect = rotated_player.get_rect(center=player.pos.center)
-        screen.blit(rotated_player, rect)
+        for player in players:
+            rotated_player = pygame.transform.rotate(player.image, player.angle)
+            rect = rotated_player.get_rect(center=player.pos.center)
+            screen.blit(rotated_player, rect)
         pygame.display.update()
         pygame.event.pump()
         pygame.time.delay(20)
