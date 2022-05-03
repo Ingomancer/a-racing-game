9a2db8a3f3fbbfa09f5ef31c296c4f9aacb8e118
Ingo Junefjord <ingo.junefjord@attentec.se>
2022-05-03 18:30:53 +0200

    You can now accelerate/decelerate, and move at small angles

--- data/car.py
+++ data/car.py
@@ -1,24 +1,32 @@
 import math
-import pygame
 
+MAX_SPEED = 8
 
 class Car:
     def __init__(self, image, controls):
         self.image = image
         self.pos = image.get_rect()
         self.angle = 0
-        self.speed = 5
+        self.speed = 0
+        self.x = self.pos.centerx
+        self.y = self.pos.centery
         self.up, self.down, self.right, self.left = controls
 
     def handle_keys(self, pressed):
         radians = math.radians(self.angle)
-        x = self.speed * math.cos(radians)
-        y = self.speed * math.sin(radians)
+        dx = self.speed * math.cos(radians)
+        dy = -self.speed * math.sin(radians)
         if pressed[self.up]:
-            self.pos = self.pos.move(x, -y)
+            if self.speed < MAX_SPEED:
+                self.speed += 1
         if pressed[self.down]:
-            self.pos = self.pos.move(-x, y)
+            if self.speed > -MAX_SPEED:
+                self.speed -= 1
         if pressed[self.right]:
             self.angle -= 5
         if pressed[self.left]:
             self.angle += 5
+        self.x += dx
+        self.y += dy
+        self.pos.centerx = int(self.x)
+        self.pos.centery = int(self.y)
